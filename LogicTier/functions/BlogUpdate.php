16b182c92664d7bfbb1b708742648b4f777ace38
wine <wineware@localhost.localdomain>
2019-04-12 14:59:07 +0200

    Don't change update button submit

--- LogicTier/functions/BlogUpdate.php
+++ LogicTier/functions/BlogUpdate.php
@@ -19,14 +19,14 @@ $inhoud = $_GET['inhoud'];
         <div class="UpdateDiv">
             <form action="" method="GET" id="editform">
                     <input type = "hidden" name = "id" value="<?php echo $_GET['id']; ?>" />
-                    <label for="TitleMessage">Title:</label><br />
+                    <label for="title">Title:</label><br />
                     <input type="text" id="InputFieldTitle" name="title" value="<?php echo $_GET['title']; ?>"><br />
-                    <label for="InhoudMessage">Bericht:</label><br />
+                    <label for="inhoud">Bericht:</label><br />
                     <textarea id="TextareaInhoud2" rows="4" cols="50" name="inhoud"><?php echo $_GET['inhoud']; ?></textarea>
                     
                     <div class="ButtonGroup">
-                    <input id="UpdateButton" type="submit" name="update" value="update"/>
-                    <button id="TerugButton" type="submit" name="update" formaction="index.php">Terug</button>
+                    <input id="UpdateButton" type="submit" name="submit" value="update"/>
+                    <button id="TerugButton" type="submit" name="back" formaction="index.php">Terug</button>
                     </div>
             </form>
         </div>
@@ -41,28 +41,21 @@ if($_GET['submit']) {
     $title = $_GET['title'];
     $inhoud = $_GET['inhoud'];
 
-    $stmt = $db_conn->prepare("UPDATE `blogPosts` SET `blogTitle` = '$title', `blogText` = '$inhoud' WHERE `blogPosts`.`blogID` = $id "); 
+    $stmt = $db_conn->prepare("UPDATE `blogPosts` SET `blogTitle` = '$title', `blogText` = '$inhoud' WHERE `blogPosts`.`blogID` = '$id' "); 
     $stmt->execute();
+    
     function Redirect($url, $permanent = false)
     {
-    if (headers_sent() === false)
-    {
-        header('Location: ' . $url, true, ($permanent === true) ? 301 : 302);
+        if (headers_sent() === false)
+            {
+                header('Location: ' . $url, true, ($permanent === true) ? 301 : 302);
+            }
+
+        exit();
     }
 
-    exit();
+    Redirect('index.php', false);
 }
-
-Redirect('index.php', false);
-}
-    // while($row = $stmt->fetch()){
-    
-    // echo "<tr><th></th><td rowspan='2'><button class='button' style='vertical-align:middle' 
-    // onclick='window.location.href=`index.php`'>Terug</button></td></tr>";
-
-
-
-    // }
     echo "</table>";
     ?>
 
